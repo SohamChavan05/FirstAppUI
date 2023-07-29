ecf4026219082b8955f702084ef1d9da85100b7d
Soham Chavan <sohamchavan159@gmail.com>
2023-07-29 16:48:17 +0530

    added two activity

--- app/src/main/java/com/example/signin_ui/MainActivity.kt
+++ app/src/main/java/com/example/signin_ui/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.signin_ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var x = findViewById<Button>(R.id.button_signin)
-        x.setOnClickListener{
-            Toast.makeText(this@MainActivity,"Enter Valid Sign In Info", Toast.LENGTH_SHORT).show()
+        var btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        btnSignIn.setOnClickListener{
+            val signIn = Intent(this,SignIn::class.java)
+            startActivity(signIn)
         }
-        var y = findViewById<Button>(R.id.button_login)
-        y.setOnClickListener{
-            Toast.makeText(this@MainActivity,"Enter Valid Log In Info", Toast.LENGTH_SHORT).show()
+
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            val logIn = Intent(this,LogIn::class.java)
+            startActivity(logIn)
         }
     }
 }
